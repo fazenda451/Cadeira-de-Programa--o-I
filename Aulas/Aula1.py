2b57ca18704cf3f91bd53be91c5468533508e314
Rodrigo Fazenda <152302205+fazenda451@users.noreply.github.com>
2025-10-31 11:08:14 +0000

    Novas pastas

---
+++ Aulas/Aula1.py
@@ -0,0 +1,13 @@
+# Variaveis
+Nome = "Rodrigo Miguel Pica da Silva Fazenda"
+Curso = "Engenharia Multimédia"
+Ano = "Ano de início: 2025-26"
+Frase = "Estou a aprender Python!"
+
+
+# Dar Print da variaveis
+print(Nome)
+print(Curso)
+print()
+print(Ano)
+print(Frase)
---
+++ Fichas/Ficha1.py
@@ -0,0 +1,9 @@
+# Variaveis
+Nome = input( 'Qual o teu nome?' )
+Curso = input( 'Qual o teu curso?' )
+Ano = input( 'Qual o teu ano Incial?' )
+Hobby = input( 'Qual o teu ano Incial?' )
+
+
+# Dar Print da variaveis
+print("Olá eu sou o ", Nome, "Sou do Curso", Curso, "do ano", Ano, "e", Hobby)
